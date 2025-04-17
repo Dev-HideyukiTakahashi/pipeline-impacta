f454cd33f4d84d804178eeee2df81be2b2b5d5fb
Hideyuki <dev.hideyukitakahashi@gmail.com>
2025-04-17 04:53:40 -0300

    projeto pipeline

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public')); 
+
+// Rota para a página inicial (front-end)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Olá github Actions! Funcionei!' });
+});
+
+app.listen(port, () => {
+  console.log(`App rodando http://localhost:${port}`);
+});
